7aa4f418edc44c3bb1927b2d37b77506d90bb5ba
archee29 <mrarchee01@gmail.com>
2022-11-02 16:54:29 +0700

    blt tahap 3

--- resources/views/layouts/user/user-layout.blade.php
+++ resources/views/layouts/user/user-layout.blade.php
@@ -4,25 +4,154 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
 
     <title>Desa Kalimas</title>
 
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    {{-- <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet"> --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500&family=Roboto:wght@400;500;700&display=swap"
+        rel="stylesheet">
 
-    <!-- Styles -->
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+
+    <link href="{{ asset('user-rsc/lib/animate/animate.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('user-rsc/lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('user-rsc/lib/lightbox/css/lightbox.min.css') }}" rel="stylesheet">
+
+
+    <link href="{{ asset('user-rsc/css/bootstrap.min.css') }}" rel="stylesheet">
+
+
+    <link href="{{ asset('user-rsc/css/style.css') }}" rel="stylesheet">
+
 </head>
 
 <body>
 
-    @extends('layouts.user.user-navbar')
+    @include('layouts.user.user-navbar')
     @yield('content')
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
-    </script>
+    <div class="container-xxl bg-white p-0">
+        <div id="spinner"
+            class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+            <div class="spinner-grow text-primary" style="width: 3rem; height: 3rem;" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+        <div class="container-fluid bg-primary text-light footer mt-5 pt-5 wow fadeIn" data-wow-delay="0.1s">
+            <div class="container py-5 px-lg-5">
+                <div class="row g-5">
+                    <div class="col-md-6 col-lg-3">
+                        <h5 class="text-white mb-4">Informasi Desa</h5>
+                        <p><i class="fa fa-map-marker-alt me-3"></i>Jalan Raya Kalimas No.26, Kalimas, Kec. Sungai
+                            Kakap, Kabupaten Kubu Raya, Kalimantan Barat 78381</p>
+                        <p><i class="fa fa-phone-alt me-3"></i>+628</p>
+                        <p><i class="fa fa-envelope me-3"></i>desa@gmail.com</p>
+                        <div class="d-flex pt-2">
+                            <a class="btn btn-outline-light btn-social" href=""><i
+                                    class="fab fa-twitter"></i></a>
+                            <a class="btn btn-outline-light btn-social" href=""><i
+                                    class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-outline-light btn-social" href=""><i
+                                    class="fab fa-youtube"></i></a>
+                            <a class="btn btn-outline-light btn-social" href=""><i
+                                    class="fab fa-instagram"></i></a>
+                            <a class="btn btn-outline-light btn-social" href=""><i
+                                    class="fa fa-envelope"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3">
+                        <h5 class="text-white mb-4">Popular Link</h5>
+                        <a class="btn btn-link" href="">About Us</a>
+                        <a class="btn btn-link" href="">Contact Us</a>
+                        <a class="btn btn-link" href="">Privacy Policy</a>
+                        <a class="btn btn-link" href="">Terms & Condition</a>
+                        <a class="btn btn-link" href="">Career</a>
+                    </div>
+                    <div class="col-md-6 col-lg-3">
+                        <h5 class="text-white mb-4">Project Gallery</h5>
+                        <div class="row g-2">
+                            <div class="col-4">
+                                <img class="img-fluid" src="img/portfolio-1.jpg" alt="Image">
+                            </div>
+                            <div class="col-4">
+                                <img class="img-fluid" src="img/portfolio-2.jpg" alt="Image">
+                            </div>
+                            <div class="col-4">
+                                <img class="img-fluid" src="img/portfolio-3.jpg" alt="Image">
+                            </div>
+                            <div class="col-4">
+                                <img class="img-fluid" src="img/portfolio-4.jpg" alt="Image">
+                            </div>
+                            <div class="col-4">
+                                <img class="img-fluid" src="img/portfolio-5.jpg" alt="Image">
+                            </div>
+                            <div class="col-4">
+                                <img class="img-fluid" src="img/portfolio-6.jpg" alt="Image">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3">
+                        <h5 class="text-white mb-4">Newsletter</h5>
+                        <p>Lorem ipsum dolor sit amet elit. Phasellus nec pretium mi. Curabitur facilisis ornare velit
+                            non vulpu</p>
+                        <div class="position-relative w-100 mt-3">
+                            <input class="form-control border-0 rounded-pill w-100 ps-4 pe-5" type="text"
+                                placeholder="Your Email" style="height: 48px;">
+                            <button type="button" class="btn shadow-none position-absolute top-0 end-0 mt-1 me-2"><i
+                                    class="fa fa-paper-plane text-primary fs-4"></i></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container px-lg-5">
+                <div class="copyright">
+                    <div class="row">
+                        <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                            &copy; <a class="border-bottom" href="#">Sistem Informasi Potensi Desa Kalimas</a>,
+                            All Right Reserved.
+                            <br>
+                            <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
+                            Designed by <a class="border-bottom" href="https://htmlcodex.com"> MBKM Smart
+                                Villagee
+                                Tanjungpura</a>
+                            <br>Distributed By: <a class="border-bottom" href="https://themewagon.com"
+                                target="_blank">Kominfo</a>
+                        </div>
+                        <div class="col-md-6 text-center text-md-end">
+                            <div class="footer-menu">
+                                <a href="">Home</a>
+                                <a href="">Cookies</a>
+                                <a href="">Help</a>
+                                <a href="">FQAs</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top pt-2"><i
+            class="bi bi-arrow-up"></i></a>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('user-rsc/lib/wow/wow.min.js') }}"></script>
+    <script src="{{ asset('user-rsc/lib/easing/easing.min.js') }}"></script>
+    <script src="{{ asset('user-rsc/lib/waypoints/waypoints.min.js') }}"></script>
+    <script src="{{ asset('user-rsc/lib/owlcarousel/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('user-rsc/lib/isotope/isotope.pkgd.min.js') }}"></script>
+    <script src="{{ asset('user-rsc/lib/lightbox/js/lightbox.min.js') }}"></script>
+    <script src="{{ asset('user-rsc/js/main.js') }}"></script>
 </body>
 
 </html>
--- resources/views/layouts/user/user-navbar.blade.php
+++ resources/views/layouts/user/user-navbar.blade.php
@@ -1,4 +1,132 @@
-<nav class="navbar navbar-dark bg-dark fixed-top">
+  <div class="container-xxl position-relative p-0">
+      <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
+          <a href="" class="navbar-brand p-0">
+              <h1 class="m-0"><i class="fa fa-search me-2"></i>SI<span class="fs-5">Mas</span></h1>
+              <!-- <img src="img/logo.png" alt="Logo"> -->
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+              <span class="fa fa-bars"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarCollapse">
+              <div class="navbar-nav ms-auto py-0">
+                  <a href="index.html" class="nav-item nav-link active">Home</a>
+                  <a href="about.html" class="nav-item nav-link">Tentang Desa</a>
+                  <a href="service.html" class="nav-item nav-link">Profile Desa</a>
+                  <div class="nav-item dropdown">
+                      <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Data Desa</a>
+                      <div class="dropdown-menu m-0">
+                          <a href="team.html" class="dropdown-item">Statistik Kependudukan</a>
+                          <a href="testimonial.html" class="dropdown-item">APBDes 2022</a>
+                      </div>
+                  </div>
+                  <div class="nav-item dropdown">
+                      <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pemerintahan</a>
+                      <div class="dropdown-menu m-0">
+                          <a href="team.html" class="dropdown-item">Visi dan Misi</a>
+                          <a href="testimonial.html" class="dropdown-item">Pemerintahan Desa</a>
+                      </div>
+                  </div>
+                  <a href="contact.html" class="nav-item nav-link">Hubungi Kami</a>
+              </div>
+              <butaton type="button" class="btn text-secondary ms-3" data-bs-toggle="modal"
+                  data-bs-target="#searchModal"><i class="fa fa-search"></i></butaton>
+              <a href="/" class="btn btn-secondary text-light rounded-pill py-2 px-4 ms-3">Peta Desa</a>
+          </div>
+      </nav>
+
+      <div class="container-xxl py-5 bg-primary hero-header mb-5">
+          <div class="container my-5 py-5 px-lg-5">
+              <div class="row g-5 py-5">
+                  <div class="col-lg-6 text-center text-lg-start">
+                      <h1 class="text-white mb-4 animated zoomIn">All in one SEO tool need to grow your business
+                          rapidly</h1>
+                      <p class="text-white pb-3 animated zoomIn">Tempor rebum no at dolore lorem clita rebum rebum
+                          ipsum rebum stet dolor sed justo kasd. Ut dolor sed magna dolor sea diam. Sit diam sit justo
+                          amet ipsum vero ipsum clita lorem</p>
+                      <a href=""
+                          class="btn btn-light py-sm-3 px-sm-5 rounded-pill me-3 animated slideInLeft">Free Quote</a>
+                      <a href=""
+                          class="btn btn-outline-light py-sm-3 px-sm-5 rounded-pill animated slideInRight">Contact
+                          Us</a>
+                  </div>
+                  <div class="col-lg-6 text-center text-lg-start">
+                      <img class="img-fluid" src="img/hero.png" alt="">
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+
+  {{-- @extends('layouts.user.user-layout')
+
+@section('content')
+    <div class="container-xxl bg-white p-0">
+        <div class="container-xxl position-relative p-0">
+            <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
+                <a href="" class="navbar-brand p-0">
+                    <h1 class="m-0"><i class="fa fa-search me-2"></i>SEO<span class="fs-5">Master</span></h1>
+                    <!-- <img src="img/logo.png" alt="Logo"> -->
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <span class="fa fa-bars"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <div class="navbar-nav ms-auto py-0">
+                        <a href="index.html" class="nav-item nav-link active">Home</a>
+                        <a href="about.html" class="nav-item nav-link">About</a>
+                        <a href="service.html" class="nav-item nav-link">Service</a>
+                        <a href="project.html" class="nav-item nav-link">Project</a>
+                        <div class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                            <div class="dropdown-menu m-0">
+                                <a href="team.html" class="dropdown-item">Our Team</a>
+                                <a href="testimonial.html" class="dropdown-item">Testimonial</a>
+                                <a href="404.html" class="dropdown-item">404 Page</a>
+                            </div>
+                        </div>
+                        <a href="contact.html" class="nav-item nav-link">Contact</a>
+                    </div>
+                    <butaton type="button" class="btn text-secondary ms-3" data-bs-toggle="modal"
+                        data-bs-target="#searchModal"><i class="fa fa-search"></i></butaton>
+                    <a href="https://htmlcodex.com/startup-company-website-template"
+                        class="btn btn-secondary text-light rounded-pill py-2 px-4 ms-3">Pro Version</a>
+                </div>
+            </nav>
+
+            <div class="container-xxl py-5 bg-primary hero-header mb-5">
+                <div class="container my-5 py-5 px-lg-5">
+                    <div class="row g-5 py-5">
+                        <div class="col-lg-6 text-center text-lg-start">
+                            <h1 class="text-white mb-4 animated zoomIn">All in one SEO tool need to grow your business
+                                rapidly</h1>
+                            <p class="text-white pb-3 animated zoomIn">Tempor rebum no at dolore lorem clita rebum rebum
+                                ipsum rebum stet dolor sed justo kasd. Ut dolor sed magna dolor sea diam. Sit diam sit justo
+                                amet ipsum vero ipsum clita lorem</p>
+                            <a href=""
+                                class="btn btn-light py-sm-3 px-sm-5 rounded-pill me-3 animated slideInLeft">Free Quote</a>
+                            <a href=""
+                                class="btn btn-outline-light py-sm-3 px-sm-5 rounded-pill animated slideInRight">Contact
+                                Us</a>
+                        </div>
+                        <div class="col-lg-6 text-center text-lg-start">
+                            <img class="img-fluid" src="img/hero.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection --}}
+
+
+
+
+
+
+
+
+
+  {{-- <nav class="navbar navbar-dark bg-dark fixed-top">
     <div class="container-fluid">
         <div>
             <a class="navbar-brand" href="#">
@@ -61,9 +189,9 @@
             </div>
         </div>
     </div>
-</nav>
+</nav> --}}
 
-{{-- <div
+  {{-- <div
     class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
     @if (Route::has('login'))
         <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
