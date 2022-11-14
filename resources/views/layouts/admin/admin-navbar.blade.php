0e48ce4014a20d5abe293c0d17ec1cc8976f1da5
archee29 <mrarchee01@gmail.com>
2022-11-14 22:50:31 +0700

    part 2

--- resources/views/layouts/admin/admin-navbar.blade.php
+++ resources/views/layouts/admin/admin-navbar.blade.php
@@ -16,8 +16,7 @@
 
         <div class="nav-item dropdown">
             <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
-                <img class="rounded-circle me-lg-2" src="admin-rsc/img/user.jpg" alt=""
-                    style="width: 40px; height: 40px;">
+                <i class="fas fa-user"></i>
                 <span class="d-none d-lg-inline-flex">{{ Auth::user()->name }}</span>
             </a>
             <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
--- resources/views/layouts/admin/admin-sidebar.blade.php
+++ resources/views/layouts/admin/admin-sidebar.blade.php
@@ -6,10 +6,8 @@
 
         <div class="d-flex align-items-center ms-4 mb-4">
             <div class="position-relative">
-                <img class="rounded-circle" src="admin-rsc/img/user.jpg" alt=""
-                    style="width: 40px; height: 40px;">
-                <div
-                    class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1">
+                <i class="fas fa-user"></i>
+                <div class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1">
                 </div>
             </div>
             <div class="ms-3">
